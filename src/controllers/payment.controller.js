2d55817bdf1977829c542802b07939200293b2bd
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2025-10-17 13:25:43 +1100

    back to normal

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -13,102 +13,6 @@ import { streamToBuffer } from "../utils/streamToBuffer.js";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const origin = process.env.ROOT_URL + "/payment-result"
 
-// export const makePayment = async (req, res, next) => {
-//   try {
-//     // const origin = req.headers.origin;
-//     const user = req.userData;
-//     const cart = await findCart(user._id);
-
-//     console.log(cart.cartItems, "cart while making the payment")
-
-//     if (!cart || cart.cartItems.length === 0) {
-//       return res.status(400).json({ status: "error", message: "Cart is empty" });
-//     }
-
-//     // Prepare line items for Stripe
-//     const line_items = cart.cartItems.map((item) => ({
-//       price_data: {
-//         currency: "aud", // or your preferred currency
-//         product_data: {
-//           name: item.name,
-//           images: item.images,
-//           metadata: {
-//             productId: String(item._id)
-//           }
-//         },
-//         unit_amount: Math.round(item.price * 100),
-//       },
-//       quantity: item.quantity,
-//     }));
-
-//     // Creates the checkout session
-//     const session = await stripe.checkout.sessions.create({
-//       payment_method_types: ["card"],
-//       line_items,
-//       customer_email: user.email,
-//       mode: "payment",
-//       success_url:
-//         "http://localhost:5173/payment-result?success=true&session_id={CHECKOUT_SESSION_ID}",
-//       cancel_url:
-//         "http://localhost:5173/payment-result?success=false&session_id={CHECKOUT_SESSION_ID}",
-
-//       // success_url: `${origin}?success=true&session_id={CHECKOUT_SESSION_ID}`,
-//       // cancel_url: `${origin}?success=false&session_id={CHECKOUT_SESSION_ID}`,
-//     });
-
-
-//     // deducting the product stock acc to the product quantity ordered
-//     for (let item of cart.cartItems) {
-//       try {
-//         const product = await getSingleProduct(item._id)
-//         console.log(product)
-
-//         if (!product) {
-//           return res.status(400).json({ status: "error", message: `Product not found in DB.` });
-//         }
-
-//         const updateProductStock = await ProductSchema.findOneAndUpdate(
-//           { _id: product._id, stock: { $gte: item.quantity } },
-//           { $inc: { stock: -Number(item.quantity) } },
-//           { new: true }
-//         )
-//         if (!updateProductStock) {
-//           return next({
-//             statusCode: 500,
-//             message: "Not Enough Stock!!",
-//           })
-//         }
-
-
-//         if (updateProductStock.stock <= 0) {
-//           await updateProductDB(updateProductStock._id, { status: "inactive" })
-//         }
-
-//       } catch (error) {
-//         return next({
-//           statusCode: 500,
-//           message: "Updating the stock failed",
-//           errorMessage: error?.message,
-//         });
-//       }
-//     }
-
-//     // Send the session URL
-//     return res.status(200).json({
-//       status: "success",
-//       message: "Checkout session created successfully",
-//       url: session.url,
-//       cart: cart,
-//     });
-//   } catch (error) {
-//     console.error("Error creating checkout session:", error);
-//     next({
-//       statusCode: 500,
-//       message: "payment failed",
-//       errorMessage: error?.message,
-//     });
-//   }
-// };
 
 export const initiatePayment = async (req, res, next) => {
   try {
@@ -145,6 +49,162 @@ export const initiatePayment = async (req, res, next) => {
     });
   }
 }
+
+export const createOrder = async (req, res, next) => {
+  try {
+    const { shippingAddress, userId, paymentIntent } = req.body
+    const user = await findUserById(userId)
+    const cart = await findCart(user._id);
+    let orderVerified = false;
+
+    // handle the stock and price 
+    // await stockHandling(cart)
+
+    // create order
+    const order = await createOrderDB({
+      products: cart.cartItems,
+      shippingAddress,
+      userId,
+      status: "pending",
+      totalAmount: cart.cartItems.reduce(
+        (sum, item) => sum + item.totalAmount,
+        0
+      ),
+      paymentDetails: paymentIntent
+    })
+
+    console.log(order, cart)
+    // creating the invoice
+    const invoice = await invoiceCreation(order, user, userId)
+
+    // Send confirmation email with PDF invoice
+    await sendConfirmationEmail(user, order, invoice)
+
+    orderVerified = true
+    return res.status(200).json({
+      verified: orderVerified,
+      message: "Verified!",
+      order
+    });
+
+  } catch (error) {
+    console.log(error?.message)
+    next({
+      statusCode: 500,
+      message: "Order Creation Failed!",
+      errorMessage: error?.message,
+    });
+  }
+}
+
+// stock handling
+export const stockHandling = async (req, res, next) => {
+  const user = req.userData
+  const cart = await findCart(user._id);
+  try {
+    if (!cart || cart.cartItems.length === 0) {
+      return next({
+        statusCode: 400,
+        message: "Cart is empty!"
+      });
+    }
+
+    // deducting the product stock acc to the product quantity ordered
+    for (let item of cart.cartItems) {
+      const product = await getSingleProduct(item._id);
+      if (!product) {
+        return next({
+          statusCode: 400,
+          message: `Product ${item._id} not found.`
+        });
+      }
+
+      const updatedProduct = await ProductSchema.findOneAndUpdate(
+        { _id: product._id, stock: { $gte: item.quantity } },
+        { $inc: { stock: -item.quantity } },
+        { new: true }
+      );
+
+      if (!updatedProduct) {
+        return next({
+          statusCode: 400,
+          message: `Not enough stock for product ${item._id}`
+        });
+      }
+
+      if (updatedProduct.stock <= 0) {
+        await updateProductDB(updatedProduct._id, { status: "inactive" });
+      }
+    }
+    return res.status(200).json({
+      status: "success",
+      message: "Stock Updated!"
+    });
+  } catch (error) {
+    next({
+      statusCode: 500,
+      message: "Stock Check Failed!",
+      errorMessage: error?.message,
+    });
+  }
+}
+
+//  invoice Creation
+const invoiceCreation = async (order, user, userId) => {
+  const invoiceNumber = generateRandomInvoiceNumber();
+  const existingInvoice = await getInvoice({ orderId: order._id });
+
+  let invoiceRecord = existingInvoice;
+
+  if (!existingInvoice) {
+    // Create and store the invoice in DB
+    invoiceRecord = await createInvoice({
+      invoiceNumber,
+      orderId: order._id,
+      userId,
+      userName: `${user.fName} ${user.lName}`,
+      totalAmount: order.totalAmount,
+      shippingAddress: order.shippingAddress,
+      taxAmount: 0,
+      status: "paid",
+      products: order.products.map(key => ({
+        id: key._id,
+        name: key.name,
+        quantity: key.quantity,
+        totalAmount: key.totalAmount,
+        productImages: key.images || []
+      })),
+      notes: order.notes || ""
+    });
+
+    await updateOrderDB(order._id, { invoiceId: invoiceRecord._id });
+  }
+
+  // Generate the PDF stream for email
+  const invoiceStream = await generateInvoice(order, invoiceRecord.invoiceNumber);
+  const invoiceBuffer = await streamToBuffer(invoiceStream);
+  return { invoiceBuffer, invoiceRecord }
+}
+
+// send email
+const sendConfirmationEmail = async (user, order, invoice) => {
+  const { invoiceBuffer, invoiceRecord } = invoice
+  await createOrderEmail({
+    userName: `${user.fName} ${user.lName}`,
+    email: user.email,
+    order,
+    attachments: [
+      {
+        filename: `invoice_${invoiceRecord.invoiceNumber}.pdf`,
+        content: invoiceBuffer,
+      },
+    ],
+  });
+
+}
+
+
+
 // export const verifyPaymentSession = async (req, res) => {
 //   const { session_id } = req.query;
 //   if (!session_id) {
@@ -369,155 +429,100 @@ export const initiatePayment = async (req, res, next) => {
 // export const refillStock = async(req)
 
 
-export const createOrder = async (req, res, next) => {
-  try {
-    const { shippingAddress, userId, paymentIntent } = req.body
-    const user = await findUserById(userId)
-    const cart = await findCart(user._id);
-    let orderVerified = false;
 
-    // handle the stock and price 
-    // await stockHandling(cart)
+// export const makePayment = async (req, res, next) => {
+//   try {
+//     // const origin = req.headers.origin;
+//     const user = req.userData;
+//     const cart = await findCart(user._id);
 
-    // create order
-    const order = await createOrderDB({
-      products: cart.cartItems,
-      shippingAddress,
-      userId,
-      status: "pending",
-      totalAmount: cart.cartItems.reduce(
-        (sum, item) => sum + item.totalAmount,
-        0
-      ),
-      paymentDetails: paymentIntent
-    })
+//     console.log(cart.cartItems, "cart while making the payment")
 
-    console.log(order, cart)
-    // creating the invoice
-    const invoice = await invoiceCreation(order, user, userId)
+//     if (!cart || cart.cartItems.length === 0) {
+//       return res.status(400).json({ status: "error", message: "Cart is empty" });
+//     }
 
-    // Send confirmation email with PDF invoice
-    await sendConfirmationEmail(user, order, invoice)
+//     // Prepare line items for Stripe
+//     const line_items = cart.cartItems.map((item) => ({
+//       price_data: {
+//         currency: "aud", // or your preferred currency
+//         product_data: {
+//           name: item.name,
+//           images: item.images,
+//           metadata: {
+//             productId: String(item._id)
+//           }
+//         },
+//         unit_amount: Math.round(item.price * 100),
+//       },
+//       quantity: item.quantity,
+//     }));
 
-    orderVerified = true
-    return res.status(200).json({
-      verified: orderVerified,
-      message: "Verified!",
-      order
-    });
+//     // Creates the checkout session
+//     const session = await stripe.checkout.sessions.create({
+//       payment_method_types: ["card"],
+//       line_items,
+//       customer_email: user.email,
+//       mode: "payment",
+//       success_url:
+//         "http://localhost:5173/payment-result?success=true&session_id={CHECKOUT_SESSION_ID}",
+//       cancel_url:
+//         "http://localhost:5173/payment-result?success=false&session_id={CHECKOUT_SESSION_ID}",
 
-  } catch (error) {
-    console.log(error?.message)
-    next({
-      statusCode: 500,
-      message: "Order Creation Failed!",
-      errorMessage: error?.message,
-    });
-  }
-}
+//       // success_url: `${origin}?success=true&session_id={CHECKOUT_SESSION_ID}`,
+//       // cancel_url: `${origin}?success=false&session_id={CHECKOUT_SESSION_ID}`,
+//     });
 
-// stock handling
-export const stockHandling = async (req, res, next) => {
-  const user = req.userData
-  const cart = await findCart(user._id);
-  try {
-    if (!cart || cart.cartItems.length === 0) {
-      return next({
-        statusCode: 400,
-        message: "Cart is empty!"
-      });
-    }
 
-    // deducting the product stock acc to the product quantity ordered
-    for (let item of cart.cartItems) {
-      const product = await getSingleProduct(item._id);
-      if (!product) {
-        return next({
-          statusCode: 400,
-          message: `Product ${item._id} not found.`
-        });
-      }
+//     // deducting the product stock acc to the product quantity ordered
+//     for (let item of cart.cartItems) {
+//       try {
+//         const product = await getSingleProduct(item._id)
+//         console.log(product)
 
-      const updatedProduct = await ProductSchema.findOneAndUpdate(
-        { _id: product._id, stock: { $gte: item.quantity } },
-        { $inc: { stock: -item.quantity } },
-        { new: true }
-      );
+//         if (!product) {
+//           return res.status(400).json({ status: "error", message: `Product not found in DB.` });
+//         }
 
-      if (!updatedProduct) {
-        return next({
-          statusCode: 400,
-          message: `Not enough stock for product ${item._id}`
-        });
-      }
+//         const updateProductStock = await ProductSchema.findOneAndUpdate(
+//           { _id: product._id, stock: { $gte: item.quantity } },
+//           { $inc: { stock: -Number(item.quantity) } },
+//           { new: true }
+//         )
+//         if (!updateProductStock) {
+//           return next({
+//             statusCode: 500,
+//             message: "Not Enough Stock!!",
+//           })
+//         }
 
-      if (updatedProduct.stock <= 0) {
-        await updateProductDB(updatedProduct._id, { status: "inactive" });
-      }
-    }
-    return res.status(200).json({
-      status: "success",
-      message: "Stock Updated!"
-    });
-  } catch (error) {
-    next({
-      statusCode: 500,
-      message: "Stock Check Failed!",
-      errorMessage: error?.message,
-    });
-  }
-}
 
-//  invoice Creation
-const invoiceCreation = async (order, user, userId) => {
-  const invoiceNumber = generateRandomInvoiceNumber();
-  const existingInvoice = await getInvoice({ orderId: order._id });
+//         if (updateProductStock.stock <= 0) {
+//           await updateProductDB(updateProductStock._id, { status: "inactive" })
+//         }
 
-  let invoiceRecord = existingInvoice;
+//       } catch (error) {
+//         return next({
+//           statusCode: 500,
+//           message: "Updating the stock failed",
+//           errorMessage: error?.message,
+//         });
+//       }
+//     }
 
-  if (!existingInvoice) {
-    // Create and store the invoice in DB
-    invoiceRecord = await createInvoice({
-      invoiceNumber,
-      orderId: order._id,
-      userId,
-      userName: `${user.fName} ${user.lName}`,
-      totalAmount: order.totalAmount,
-      shippingAddress: order.shippingAddress,
-      taxAmount: 0,
-      status: "paid",
-      products: order.products.map(key => ({
-        id: key._id,
-        name: key.name,
-        quantity: key.quantity,
-        totalAmount: key.totalAmount,
-        productImages: key.images || []
-      })),
-      notes: order.notes || ""
-    });
-
-    await updateOrderDB(order._id, { invoiceId: invoiceRecord._id });
-  }
-
-  // Generate the PDF stream for email
-  const invoiceStream = await generateInvoice(order, invoiceRecord.invoiceNumber);
-  const invoiceBuffer = await streamToBuffer(invoiceStream);
-  return { invoiceBuffer, invoiceRecord }
-}
-
-// send email
-const sendConfirmationEmail = async (user, order, invoice) => {
-  const { invoiceBuffer, invoiceRecord } = invoice
-  await createOrderEmail({
-    userName: `${user.fName} ${user.lName}`,
-    email: user.email,
-    order,
-    attachments: [
-      {
-        filename: `invoice_${invoiceRecord.invoiceNumber}.pdf`,
-        content: invoiceBuffer,
-      },
-    ],
-  });
-
-}
+//     // Send the session URL
+//     return res.status(200).json({
+//       status: "success",
+//       message: "Checkout session created successfully",
+//       url: session.url,
+//       cart: cart,
+//     });
+//   } catch (error) {
+//     console.error("Error creating checkout session:", error);
+//     next({
+//       statusCode: 500,
+//       message: "payment failed",
+//       errorMessage: error?.message,
+//     });
+//   }
+// };
